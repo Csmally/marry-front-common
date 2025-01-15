b4fb628944446d77ec6f3dcfbf78dacf7ea765e6
yangxuan <1130180692@qq.com>
2025-01-15 19:39:11 +0800

    feat: 抽奖框架

--- src/components/LottiePlayer/index.tsx
+++ src/components/LottiePlayer/index.tsx
@@ -1,17 +1,19 @@
-import { memo } from "react"
+import { forwardRef, memo } from "react"
 import Lottie, { LottieProps } from "react-lottie";
 
 export interface LottiePlayerPropsType extends LottieProps {
     className?: string;
 }
 
-const LottiePlayer: React.FC<LottiePlayerPropsType> = (props) => {
+const LottiePlayer = forwardRef<Lottie, LottiePlayerPropsType>((props, ref) => {
     const { className = '', ...other} = props;
     return (
         <div className={className}>
-            <Lottie {...other}/>
+            <Lottie {...other} ref={ref}/>
         </div>
-    )
-}
+    );
+})
+
+LottiePlayer.displayName = 'LottiePlayer';
 
 export default memo(LottiePlayer);
--- src/pages/GiftPage/index.tsx
+++ src/pages/GiftPage/index.tsx
@@ -1,17 +1,17 @@
-import { memo } from "react";
+import { memo, useRef } from "react";
 import * as styles from "./styles.module.css";
 import LottiePlayer, { LottiePlayerPropsType } from "@/components/LottiePlayer";
 import planeHeart from "@/assets/lottieJson/planeHeart.json";
-// import moreHeart from "@/assets/lottieJson/moreHeart.json";
 import heartGift from "@/assets/lottieJson/heartGift.json";
 import giftMachine from "@/assets/lottieJson/giftMachine.json";
 import chatHeart from "@/assets/lottieJson/chatHeart.json";
 import photoCamera from "@/assets/lottieJson/photoCamera.json";
 import giftBox from "@/assets/lottieJson/giftBox.json";
+import loveText from "@/assets/lottieJson/loveText.json";
+import loveChat1 from "@/assets/lottieJson/loveChat1.json";
 
 // 右上角桃心飞机
 const planeHeartProps: LottiePlayerPropsType = {
-  className: styles.planeHeartBox,
   options: {
     animationData: planeHeart,
   },
@@ -19,9 +19,8 @@ const planeHeartProps: LottiePlayerPropsType = {
   height: 150,
 };
 
-// 右下角桃心礼盒
+// 左下角桃心礼盒
 const heartGiftProps: LottiePlayerPropsType = {
-  className: styles.heartGiftBox,
   options: {
     animationData: heartGift,
   },
@@ -37,15 +36,6 @@ const chatHeartProps: LottiePlayerPropsType = {
   },
   width: 100,
   height: 100,
-};
-
-// 左边-弹幕-抽奖机
-const giftMachineLProps: LottiePlayerPropsType = {
-  options: {
-    animationData: giftMachine,
-  },
-  width: "70vh",
-  height: "70vh",
 };
 
 // 相机拍照
@@ -68,29 +58,74 @@ const giftMachineRProps: LottiePlayerPropsType = {
 };
 
 const giftBoxProps: LottiePlayerPropsType = {
-  className: styles.giftBoxBox,
   options: {
     animationData: giftBox,
   },
-  width: 300,
-  height: 300,
+  width: 150,
+  height: 150,
+};
+
+const loveTextProps: LottiePlayerPropsType = {
+  options: {
+    animationData: loveText,
+  },
+  width: 150,
+  height: 150,
+};
+
+const loveChat1Props: LottiePlayerPropsType = {
+  options: {
+    animationData: loveChat1,
+  },
+  width: 200,
+  height: 200,
 };
 
 const GiftPage: React.FC = () => {
+  const machnieRef = useRef(null);
+  // 左边-弹幕-抽奖机
+  const giftMachineLProps: LottiePlayerPropsType = {
+    options: {
+      animationData: giftMachine,
+      autoplay: false,
+    },
+    width: "70vh",
+    height: "70vh",
+    eventListeners: [
+      {
+        eventName: 'loopComplete',
+        callback: () => {
+          machnieRef.current.stop();
+        }
+      }
+    ]
+  };
+  const tt = () => {
+    machnieRef.current.play();
+  };
   return (
     <div className={styles.container}>
-      <LottiePlayer {...planeHeartProps} />
-      <LottiePlayer {...heartGiftProps} />
-      <LottiePlayer {...giftBoxProps} />
+      <div className={styles.topAnimationBar}>
+        <LottiePlayer {...loveTextProps} />
+        <LottiePlayer {...loveChat1Props} />
+        <LottiePlayer {...planeHeartProps} />
+        <div style={{background: 'red'}} onClick={tt}>点击</div>
+      </div>
+      
 
       {/* 中间主图 */}
       <div className={styles.giftMachineContainer}>
         <LottiePlayer {...chatHeartProps} />
-        <LottiePlayer {...giftMachineLProps} />
+        <LottiePlayer {...giftMachineLProps} ref={machnieRef}/>
       </div>
       <div className={styles.giftMachineContainer}>
         <LottiePlayer {...photoCameraProps} />
         <LottiePlayer {...giftMachineRProps} />
+      </div>
+      
+      <div className={styles.bottomAnimationBar}>
+        <LottiePlayer {...heartGiftProps} />
+        <LottiePlayer {...giftBoxProps} />
       </div>
     </div>
   );
