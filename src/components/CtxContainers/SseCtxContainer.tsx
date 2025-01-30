f8811a525daed4fd3d8f86516173f3022524e712
Csmally <1130180692@qq.com>
2025-01-31 02:21:20 +0800

    feat: 增加sse重连机制

--- src/components/CtxContainers/SseCtxContainer.tsx
+++ src/components/CtxContainers/SseCtxContainer.tsx
@@ -1,19 +1,26 @@
 import { ReactFCPropsType } from "@/types/main";
 import { SseCtx } from "@/utils/ctxs";
-import { memo, useEffect, useState } from "react";
+import { memo, useCallback, useEffect, useState } from "react";
 import { v4 as uuidv4 } from "uuid";
 
 const SseCtxContainer: React.FC<ReactFCPropsType> = (props) => {
   const { children } = props;
   const [sseClient, setSseClient] = useState(null);
-  useEffect(() => {
+  const connectSSE = useCallback(() => {
     // 创建 EventSource 实例
     const eventSource = new EventSource(
       `${process.env.BASE_URL}common/sse?uuid=${uuidv4()}`
     );
-    setSseClient(eventSource);
-    return () => {
+    eventSource.onerror = () => {
       eventSource.close();
+      connectSSE();
+    };
+    setSseClient(eventSource);
+  }, []);
+  useEffect(() => {
+    connectSSE();
+    return () => {
+      sseClient?.close?.();
     };
   }, []);
   const initCtxData = { sseClient };
