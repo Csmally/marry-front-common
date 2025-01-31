b89c5595727052e0af32fe1d04274e9980935901
Csmally <1130180692@qq.com>
2025-01-31 17:10:16 +0800

    feat: 相册桃心外形

--- src/components/CtxContainers/PhotoCtxContainer.tsx
+++ src/components/CtxContainers/PhotoCtxContainer.tsx
@@ -2,16 +2,44 @@ import { ReactFCPropsType } from "@/types/main";
 import { PhotoCtx, SseCtx } from "@/utils/ctxs";
 import { memo, useContext, useEffect, useState } from "react";
 
+const initPhotos = [
+  "https://www.onelight.ink/assets/movieScreen/movieScreen1.webp",
+  "https://www.onelight.ink/assets/movieScreen/movieScreen2.webp",
+  "https://www.onelight.ink/assets/movieScreen/movieScreen3.webp",
+  "https://www.onelight.ink/assets/movieScreen/movieScreen4.webp",
+  "https://www.onelight.ink/assets/movieScreen/movieScreen5.webp",
+  "https://www.onelight.ink/assets/movieScreen/movieScreen6.webp",
+  "https://www.onelight.ink/assets/movieScreen/movieScreen7.webp",
+  "https://www.onelight.ink/assets/movieScreen/movieScreen8.webp",
+  "https://www.onelight.ink/assets/movieScreen/movieScreen9.webp",
+  "https://www.onelight.ink/assets/movieScreen/movieScreen10.webp",
+  "https://www.onelight.ink/assets/movieScreen/movieScreen11.webp",
+  "https://www.onelight.ink/assets/movieScreen/movieScreen12.webp",
+  "https://www.onelight.ink/assets/movieScreen/movieScreen13.webp",
+  "https://www.onelight.ink/assets/movieScreen/movieScreen14.webp",
+  "https://www.onelight.ink/assets/movieScreen/movieScreen15.webp",
+  "https://www.onelight.ink/assets/movieScreen/movieScreen16.webp",
+  "https://www.onelight.ink/assets/movieScreen/movieScreen17.webp",
+  "https://www.onelight.ink/assets/movieScreen/movieScreen18.webp",
+  "https://www.onelight.ink/assets/movieScreen/movieScreen19.webp",
+  "https://www.onelight.ink/assets/movieScreen/movieScreen20.webp",
+  "https://www.onelight.ink/assets/movieScreen/movieScreen21.webp",
+  "https://www.onelight.ink/assets/movieScreen/movieScreen22.webp",
+  "https://www.onelight.ink/assets/movieScreen/movieScreen23.webp",
+  "https://www.onelight.ink/assets/movieScreen/movieScreen24.webp",
+  "https://www.onelight.ink/assets/movieScreen/movieScreen25.webp",
+  "https://www.onelight.ink/assets/movieScreen/movieScreen26.webp",
+];
+
 const PhotoCtxContainer: React.FC<ReactFCPropsType> = (props) => {
   const { children } = props;
   const { sseClient } = useContext(SseCtx);
-  const [photos, setPhotos] = useState([]);
+  const [photos, setPhotos] = useState(initPhotos);
   useEffect(() => {
     if (sseClient) {
       // 监听自定义发送弹幕消息事件
       sseClient.addEventListener("addPhoto", (event) => {
-        console.log("9898--addPhoto--来了", event);
-        setPhotos((i) => [...i, 123]);
+        setPhotos((i) => [...i, event.data]);
       });
     }
   }, [sseClient]);
--- src/pages/PhotoPage/index.tsx
+++ src/pages/PhotoPage/index.tsx
@@ -1,16 +1,105 @@
 import { PhotoCtx } from "@/utils/ctxs";
-import { memo, useContext } from "react";
+import { memo, useCallback, useContext } from "react";
+import * as styles from "./styles.module.css";
+import LottiePlayer, { LottiePlayerPropsType } from "@/components/LottiePlayer";
+import photo1Json from "@/assets/lottieJson/photo1.json";
+import photo2Json from "@/assets/lottieJson/photo2.json";
+import photo3Json from "@/assets/lottieJson/photo3.json";
+import photo4Json from "@/assets/lottieJson/photo4.json";
+import photo5Json from "@/assets/lottieJson/photo5.json";
+import photo6Json from "@/assets/lottieJson/photo6.json";
+import photo7Json from "@/assets/lottieJson/photo7.json";
+
+const photo1Props: LottiePlayerPropsType = {
+  options: {
+    animationData: photo1Json,
+  },
+  width: 100,
+  height: 100,
+};
+const photo2Props: LottiePlayerPropsType = {
+  options: {
+    animationData: photo2Json,
+  },
+  width: 100,
+  height: 100,
+};
+const photo3Props: LottiePlayerPropsType = {
+  options: {
+    animationData: photo3Json,
+  },
+  width: 150,
+  height: 150,
+};
+const photo4Props: LottiePlayerPropsType = {
+  options: {
+    animationData: photo4Json,
+  },
+  width: 100,
+  height: 100,
+};
+const photo5Props: LottiePlayerPropsType = {
+  options: {
+    animationData: photo5Json,
+  },
+  width: 150,
+  height: 150,
+};
+const photo6Props: LottiePlayerPropsType = {
+  options: {
+    animationData: photo6Json,
+  },
+  width: 100,
+  height: 100,
+};
+
+const photo7Props: LottiePlayerPropsType = {
+  options: {
+    animationData: photo7Json,
+  },
+  width: 100,
+  height: 100,
+};
 
 const PhotoPage: React.FC = () => {
   const { photos } = useContext(PhotoCtx);
+  const getRandomRotate = useCallback(() => {
+    return `rotate(${Math.floor(Math.random() * 61) - 30}deg)`;
+  }, []);
   return (
-    <div style={{ background: "red" }}>
-      {photos.map((i, index) => (
-        <div key={index} style={{ height: 100, background: "pink" }}>
-          {i}
+    <>
+      <div className={styles.container}>
+        <div className={styles.topAnimationBar}>
+          <LottiePlayer {...photo1Props} />
+          <LottiePlayer {...photo2Props} />
+          <LottiePlayer {...photo2Props} />
+          <LottiePlayer {...photo2Props} />
+          <LottiePlayer {...photo5Props} />
         </div>
-      ))}
-    </div>
+
+        <div className={styles.midAnimationBar}>
+          <LottiePlayer {...photo7Props} />
+          <LottiePlayer {...photo7Props} />
+        </div>
+        <div className={styles.bottomAnimationBar}>
+          <LottiePlayer {...photo3Props} />
+          <LottiePlayer {...photo6Props} />
+          <LottiePlayer {...photo4Props} />
+        </div>
+      </div>
+      <div className={styles.photoContainer}>
+        <div className={styles.photoBox}>
+          {photos.map((i, index) => (
+            <img
+              key={index}
+              src={i}
+              className={styles.photoImg}
+              style={{ transform: getRandomRotate() }}
+            />
+          ))}
+        </div>
+      </div>
+    </>
   );
 };
 
